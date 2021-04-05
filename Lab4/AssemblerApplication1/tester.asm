7a3f0249bd82dfdb446cdf0a49b13b9a5490cdef
trbutler4 <58192340+trbutler4@users.noreply.github.com>
2021-04-05 16:19:56 -0500

    updated tester to switch between low and high brightness levels

--- Lab4/AssemblerApplication1/tester.asm
+++ Lab4/AssemblerApplication1/tester.asm
@@ -1,33 +1,3 @@
-;
-; lab4-misc.asm
-;
-; Created: 4/4/2021 9:22:55 PM
-; Author : tombu
-;
-
-
-; PD5 - PWM pin for LEDS
-
-; PD2 - External Interupt 0 for PBS
-
-; sei/cli  -> gloabally enable/disable interrupts
-
-;;;;;;;;;;;;;;;;;
-; from lecture example
-; TCTN0 -> 256 - n = 28
-; CS02: c00 = 100
-; normal mode -> WGM02: WGM00 = 000
-; TCCR0A = 0x00
-; TCCR0B = 0x04
-
-
-;;;;;;;;;;;;;;;;
-; Example ISR
-; extint: push r0 ; save r0 on stack
-	;     ...
-	;	  pop r0 ; restore r0
-	;	  reti   ; return and enable interrupts
-
 .include "m328Pdef.inc"
 .def tmp1 = r23 ; Use r23 for temporary variables
 .def tmp2 = r24 ; use r24 for temporary values
@@ -45,34 +15,41 @@ sbi DDRD,5 ; set pin as output
 ; sei/cli  -> gloabally enable/disable interrupts
 
 
-; configure TIMER0 as PWM ( not correct )
+; configure TIMER0 as PWM 
 ldi tmp1,0b00110011 ; fast PWM mode, inverting mode (sets OC0b on compare match)
 out TCCR0A, tmp1 
-ldi tmp1,0x01 ; no prescale (change for PWM?)
+ldi tmp1,0b00001001 ; no prescale, WGM02 = 1 
 out TCCR0B,tmp1 
-ldi tmp1,0x00 ; (change for PWM?)
+ldi tmp1,0xff ; (change for PWM?)
 out TCNT0,tmp1
+;ldi tmp1, 0x01  ; 00-ff adjusts brightness level
+;out OCR0A,tmp1
+
 
 main:
-	;rcall delay -- LEDS on without calling delay (change to non-inverting to turn off?)
+	ldi tmp1,0x01 ; low brightness
+	out OCR0A,tmp1
+
+	rcall delay_long
+
+	ldi tmp1,0xff ; high brightness
+	out OCR0A,tmp1
+
+	rcall delay_long
+	
 
 	rjmp main
 
-
-
-delay_long:
-    rcall delay
-    sbiw Z,1
-    brne delay_long
-    ret
-
-delay:
-    in tmp2,TCNT0 ; Get counter value 
-    cpi tmp2,0x00 ; is it zero?
-    brne delay ; no --> wait more
-
-
-
-
-
-
+ delay_long:
+      ldi   r23,255      ; r23 <-- Counter for outer loop
+  d1: ldi   r24,253     ; r24 <-- Counter for level 2 loop 
+  d2: ldi   r25,253     ; r25 <-- Counter for inner loop
+  d3: dec   r25
+      nop               ; no operation 
+      brne  d3 
+      dec   r24
+      brne  d2
+      dec   r23
+      brne  d1
+      ret
+.exit
