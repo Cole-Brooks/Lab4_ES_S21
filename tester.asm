b4770f798e5ae61ce5d803416da7c53f21d8c752
trbutler4 <58192340+trbutler4@users.noreply.github.com>
2021-04-05 19:08:23 -0500

    moved tester file, which now contains lights alternating from 5-50-100% duty cycle

--- tester.asm
+++ tester.asm
@@ -16,32 +16,34 @@ sbi DDRD,5 ; set pin as output
 
 
 ; configure TIMER0 as PWM 
-ldi tmp1,0b00110011 ; fast PWM mode, inverting mode (sets OC0b on compare match)
+ldi tmp1,0b00100011 ; fast PWM mode, non-inverting mode 
 out TCCR0A, tmp1 
-ldi tmp1,0b00001001 ; no prescale, WGM02 = 1 
+ldi tmp1,0b00001011 ; 64 prescale, WGM02 = 1 
 out TCCR0B,tmp1 
-ldi tmp1,0xff ; (change for PWM?)
-out TCNT0,tmp1
-;ldi tmp1, 0x01  ; 00-ff adjusts brightness level
-;out OCR0A,tmp1
-
+ldi tmp1,100  ; TOP = 100
+out OCR0A,tmp1
 
 main:
-	ldi tmp1,0x01 ; low brightness
-	out OCR0A,tmp1
+	ldi tmp1,5 ; low brightness
+	out OCR0B,tmp1
 
 	rcall delay_long
 
-	ldi tmp1,0xff ; high brightness
-	out OCR0A,tmp1
+	ldi tmp1,50 ; mid brightness
+	out OCR0B,tmp1
 
 	rcall delay_long
-	
+
+	ldi tmp1,100 ; high brightness
+	out OCR0B,tmp1
+
+	rcall delay_long
+
 
 	rjmp main
 
  delay_long:
-      ldi   r23,255      ; r23 <-- Counter for outer loop
+      ldi   r23,100      ; r23 <-- Counter for outer loop
   d1: ldi   r24,253     ; r24 <-- Counter for level 2 loop 
   d2: ldi   r25,253     ; r25 <-- Counter for inner loop
   d3: dec   r25
